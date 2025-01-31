74d02a87605f266b756cd630d0f29c8b6286876e
richmondafari <afaririchmond1000@gmail.com>
2025-01-31 16:51:01 +0000

    Built the header

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,18 @@
 import { HomePageContainer } from "../styles/HomePage.styled";
+
+
+
 const HomePage = () => {
-  return (
-    <HomePageContainer>
-      <div className="heading">IP Address Tracker</div>
-    </HomePageContainer>
-  );
+  
+
+    return (
+      <HomePageContainer>
+        <div className="background-image">
+          <img src="" alt="Background" />
+        </div>
+        <div className="heading">IP Address Tracker</div>
+      </HomePageContainer>
+    );
 };
 
 export default HomePage;
--- src/styles/HomePage.styled.jsx
+++ src/styles/HomePage.styled.jsx
@@ -3,6 +3,13 @@ import styles from 'styled-components';
 
 export const HomePageContainer = styles.div`
 
+display: flex;
+align-items: center;
+justify-content: center;
+background-color: #f6f6f7;
+
+
+
 
 
 `
