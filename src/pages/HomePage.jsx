aeb672b122d738a4c717200c13d6d10ef892b56a
richmondafari <afaririchmond1000@gmail.com>
2025-01-31 17:51:04 +0000

    Fixed the image display issue

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,18 +1,14 @@
 import { HomePageContainer } from "../styles/HomePage.styled";
 
-
-
 const HomePage = () => {
-  
-
-    return (
-      <HomePageContainer>
-        <div className="background-image">
-          <img src="" alt="Background" />
-        </div>
+  return (
+    <HomePageContainer>
+      <div className="background">
         <div className="heading">IP Address Tracker</div>
-      </HomePageContainer>
-    );
+      </div>
+      
+    </HomePageContainer>
+  );
 };
 
 export default HomePage;
--- src/styles/HomePage.styled.jsx
+++ src/styles/HomePage.styled.jsx
@@ -1,15 +1,19 @@
-import styles from 'styled-components';
-
+import styles from "styled-components";
 
 export const HomePageContainer = styles.div`
 
 display: flex;
 align-items: center;
 justify-content: center;
-background-color: #f6f6f7;
+background-image: url('./src/assets/images/pattern-bg-desktop.png'); 
+width: 100%;
+height: 200px;
+background-size: cover;
+background-position: center;
 
 
 
 
 
-`
+
+`;
