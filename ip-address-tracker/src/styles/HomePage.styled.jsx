007063c4b975b76a345638654c2a014c0482d0b9
richmondafari <afaririchmond1000@gmail.com>
2025-02-14 18:03:08 +0000

    Updated the background image

--- ip-address-tracker/src/styles/HomePage.styled.jsx
+++ ip-address-tracker/src/styles/HomePage.styled.jsx
@@ -1,20 +1,15 @@
 import styles from "styled-components";
 
+import bgImage from "/src/assets/images/pattern-bg-desktop.png";
+
 export const HomePageContainer = styles.div`
-
-display: flex;
-flex-direction: column;
-align-items: center;
-justify-content: center;
-background-image: url('./src/assets/images/pattern-bg-desktop.png'); 
-width: 100%;
-height: 250px;
-background-size: cover;
-background-position: center;
-
-
-
-
-
-
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-image: url(${bgImage});
+  width: 100%;
+  height: 250px;
+  background-size: cover;
+  background-position: center;
 `;
